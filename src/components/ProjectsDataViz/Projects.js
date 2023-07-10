3615fba9ec79e672d037e6c1f5d4786a960d1088
Adi Kelvianto <adikelvianto2906@gmail.com>
2023-07-10 22:32:28 +0700

    test

--- src/components/ProjectsDataViz/Projects.js
+++ src/components/ProjectsDataViz/Projects.js
@@ -5,10 +5,8 @@ import Button from "react-bootstrap/Button";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
 import editor from "../../Assets/Projects/codeEditor.png";
 import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
 
 import Card from "react-bootstrap/Card";
