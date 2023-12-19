4dc1351251019307cf4511618a39f093523484a0
Adi Kelvianto <adikelvianto2906@gmail.com>
2023-12-19 19:15:52 +0700

    update project icon

--- src/components/ProjectList/ProjectList.js
+++ src/components/ProjectList/ProjectList.js
@@ -6,9 +6,9 @@ import { Link } from "react-router-dom";
 function ProjectList(props) {
   return (
     <Card className="project-card-view">
-    <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+    <Card.Img variant="top" src={props.imgPath} alt="card-img" style={{ width: '200px', margin: '0 auto', display: 'block' }} />
     <Card.Body>
-    <Card.Title>{props.title}</Card.Title>
+    <Card.Title><b>{props.title}</b></Card.Title>
           <Card.Text style={{ textAlign: "center" }}>
             {props.description}
           </Card.Text>
--- src/components/ProjectList/Projects.js
+++ src/components/ProjectList/Projects.js
@@ -2,7 +2,9 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Projectlist from "./ProjectList";
 import Particle from "../Particle";
-import chatify from "../../Assets/Projects/chatify.png";
+import dataviz_icon from "../../Assets/Projects/Data Viz Icon.png";
+import datascience_icon from "../../Assets/Projects/Data Science Icon.png";
+import backend_icon from "../../Assets/Projects/Back End Icon.png";
 
 function ProjectList() {
   return (
@@ -18,7 +20,7 @@ function ProjectList() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <Projectlist
-              imgPath={chatify}
+              imgPath={dataviz_icon}
               title="Data Processing and Visualization"
               description="Gathering, cleansing, and visualize data"
               link="/project_dataviz"
@@ -27,7 +29,7 @@ function ProjectList() {
 
           <Col md={4} className="project-card">
             <Projectlist
-              imgPath={chatify}
+              imgPath={datascience_icon}
               title="Data Science"
               description="Gathering, cleansing, and visualize data"
               link="/project_datascience"
@@ -35,7 +37,7 @@ function ProjectList() {
           </Col>
           <Col md={4} className="project-card">
             <Projectlist
-              imgPath={chatify}
+              imgPath={backend_icon}
               title="Back End"
               description="Gathering, cleansing, and visualize data"
               link="/project_backend"
